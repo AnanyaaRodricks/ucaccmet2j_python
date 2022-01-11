dc8881e429a184518a00a50631d4b75566ec79d4
AnanyaaRodricks <97510141+AnanyaaRodricks@users.noreply.github.com>
2022-01-11 17:14:47 +0100

    added comments to my code

--- Weather_part2.py
+++ Weather_part2.py
@@ -1,16 +1,18 @@
+# empty set called rate is created
 rate = []
 
 import json
 with open ('result1.json') as file:
     data = json.load(file)
-print(data)
-yearly_rain = sum(data)
-print(yearly_rain)
+# Made a variable where the monthly precipitation values were summed
+    yearly_rain = sum(data)
 for x in data:
+# divided each element of the list by the yearly sum of rain
    relativerain = (x/yearly_rain)
    relativerain = round(relativerain,2)
    rate.append(relativerain)
 print(rate)
 
+# Saved the list as a Json file
 with open('result2.json', 'w') as relativerain_file:
         json.dump( rate, relativerain_file)
